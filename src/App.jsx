963ab22e123c6d37c84c32f219dce3e72fbfbc7d
kidsora <mohamed666abassi@gmail.com>
2025-06-23 13:29:23 +0100

    files changed

--- src/App.jsx
+++ src/App.jsx
@@ -5,11 +5,19 @@ import LoginPage from "./pages/LoginPage";
 import ProfilePage from "./pages/ProfilePage";
 import { Toaster } from "react-hot-toast";
 import { AuthContext } from "../context/AuthContext";
+import bgImage from "./assets/bgImage.svg"; // ✅ imported
 
 const App = () => {
   const { authUser } = useContext(AuthContext);
   return (
-    <div className="bg-[url('./src/assets/bgImage.svg')] bg-contain">
+    <div
+      style={{
+        backgroundImage: `url(${bgImage})`,
+        backgroundSize: "contain",
+        backgroundRepeat: "no-repeat",
+        minHeight: "100vh",
+      }}
+    >
       <Toaster />
       <Routes>
         <Route
